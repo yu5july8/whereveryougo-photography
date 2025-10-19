b8fc29e65f2748d7add6dbafed9ec4c7cfa9745e
yu5july8 <124512658+yu5july8@users.noreply.github.com>
2025-10-19 10:54:45 -0400

    add contents

---
+++ assets/scripts/scrips.js
@@ -0,0 +1,6 @@
+const menuIcon = document.getElementById("menu-icon");
+const navLinks = document.getElementById("nav-links");
+
+menuIcon.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
